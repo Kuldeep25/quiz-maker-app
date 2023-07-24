d14359a87f5176df9b1eac05fe6abc12d3882527
kuldeep <kamblekuldeep25@gmail.com>
2023-07-24 18:27:31 +0530

    fix to work in stackbliz as its working locally fine but not in stackbliz added zone.js in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+//fix to work in stackbliz as its working locally fine but not in stackbliz
+import 'zone.js'
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
